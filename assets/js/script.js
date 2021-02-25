ac87ebfd5cfa4223dd8376526d15a7d9207cef63
ezzeldem <ezzelden06@gmail.com>
2021-02-25 10:36:19 +0200

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 
- 
+    $('.logIn-form button').click(function(){
+        if ($('.logIn-form input#userName_input').val() == "" || $('.logIn-form input#password_input').val() == "" )  {
+            $(".wrong-label").show();
+        }
+        
+        if(){
+            
+        }
+    });
+
 });
