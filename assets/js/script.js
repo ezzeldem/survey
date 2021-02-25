6a364240333f2be00fa6ccc8e3473406bc262d80
ezzeldem <ezzelden06@gmail.com>
2021-02-25 13:02:02 +0200

    ezz

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,6 @@ $(document).ready(function(){
         if ($('.logIn-form input#userName_input').val() == "" || $('.logIn-form input#password_input').val() == "" )  {
             $(".wrong-label").show();
         }
-        
-        if(){
-            
-        }
     });
 
 });
